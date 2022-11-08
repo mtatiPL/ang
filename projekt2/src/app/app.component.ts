aabc221482c136f8ba631180a194485be93787f3
unknown <you@example.com>
2022-11-08 09:28:45 +0100

    2

--- projekt2/src/app/app.component.ts
+++ projekt2/src/app/app.component.ts
@@ -1,32 +1,48 @@
 import { Component } from '@angular/core';
-
+import { Osoba } from "./interfejs";
 @Component({
   selector: 'app-root',
   template:`
   <p [style.background-color]="kolor" [style.text-align]="jak"> Firma {{ nazwa }}</p>
-
-  <img  src="assets/images/obraz_1.jpg" [class.zdjecie]="aktywna">
-
-  <button  (click)="zmienKolor()"  [style.background-color]="kolor" type="button" [class.przycisk]="aktywna">Zmień kolor tła</button>`,
+  <label> <input type="checkbox" [(ngModel)]=pokazZdjecie> Pokaz Zdjecie</label>
+  <div id='ramka'>
+    <img src={{osoba.zdjecie}} [class.zdjecie]="aktywna" [style.zdjecie]="nazwa" *ngIf="pokazZdjecie else brakZdjecia"> 
+  </div>
+  <div id='kontakt' class='tekst'>
+    <p>Kontakt:</p>
+      <ul>
+        <li *ngFor="let value of osoba.adres">{{value}}</li>
+      </ul>
+  </div>
+  <button  (click)="zmienKolor()"  [style.background-color]="kolor" type="button" [class.przycisk]="aktywna">Zmień kolor tła</button>
+  <ng-template #brakZdjecia>
+    <img src='{{osoba.bezZdjecia}}' class='zdjecie'>
+    <p class='tekst_srodek'>Brak zdjecia</p>
+  </ng-template>`,
   
-
-  
+  styleUrls:['app.component.css'],
 styles: [`
 .zdjecie {border: 3px solid #000; display: block; margin: auto; }
   .przycisk {border: none; color: blue;  background-color: red; padding: 20px; font-size: 10px; dissplay:block; margin: 10px auto;}
   .przycisk:hover {background: #eee;}`]
-  
 })
  
 
 
 export class AppComponent {
-
+osoba: Osoba={
+  imie: "Mateusz",
+  nazwisko: "Jakis",
+  zdjecie: "assets/images/obraz_1.jpg",
+  bezZdjecia: "assets/images/nofoto.jpg",
+  adres: ["Chopina 26","41-807","Zabrze"]
+  
+}
 nazwa:string="FedEx"
   kolor: string = "red";
   jak: string = "center";
   aktywna: boolean = true;
-
+  pokazZdjecie: boolean = false;
   zmienKolor() {
     this.kolor = this.kolor === "red" ? "blue" : "red";
   }
---
+++ projekt2/src/app/interfejs.ts
@@ -0,0 +1,8 @@
+export interface Osoba {
+    imie:string;
+    nazwisko: string;
+    zdjecie: string;
+    bezZdjecia: string;
+    adres: string[];
+  }
+  
